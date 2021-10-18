fcdcaaaa3c9fd83f6aeaeaf4e579467b2cfbd4ae
Nam Huynh <namhq.1989@gmail.com>
2021-10-18 14:42:07 +0700

    update config struct

--- config.go
+++ config.go
@@ -3,18 +3,25 @@ package queue
 // Config ...
 type Config struct {
 	// For message queue
-	Redis struct {
-		URL      string
-		Password string
-	}
+	Redis ConfigRedis
+
 	// Priority to process task, eg: Critical 6, Default 3, Low 1
 	// Using for server only
 	// https://github.com/hibiken/asynq/wiki/Queue-Priority
 	Concurrency int
-	Priority    struct {
-		Critical   int
-		Default    int
-		Low        int
-		StrictMode bool
-	}
+	Priority    ConfigPriority
+}
+
+// ConfigRedis ...
+type ConfigRedis struct {
+	URL      string
+	Password string
+}
+
+// ConfigPriority ...
+type ConfigPriority struct {
+	Critical   int
+	Default    int
+	Low        int
+	StrictMode bool
 }
